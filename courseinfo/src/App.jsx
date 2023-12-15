3b7eb20ea8c4d00aa9f55db5a10127e15e9e8f35
tastas000 <TiiLg0o000@gmail.com>
2023-12-15 12:11:14 +0700

    finish ex1.3

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -4,14 +4,18 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.content[0].part} exercises={props.content[0].exercises} />
-      <Part part={props.content[1].part} exercises={props.content[1].exercises} />
-      <Part part={props.content[2].part} exercises={props.content[2].exercises} />
+      <Part part={props.part1.name} exercises={props.part1.exercises} />
+      <Part part={props.part2.name} exercises={props.part2.exercises} />
+      <Part part={props.part3.name} exercises={props.part3.exercises} />
     </div>
   );
 };
 const Part = (props) => {
-  return <p>{props.part} {props.exercises}</p>;
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
 };
 const Total = (props) => {
   return <p>Number of exercises {props.total}</p>;
@@ -19,24 +23,24 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10,
+  };
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7,
+  };
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14,
+  };
 
   return (
     <>
       <Header course={course} />
-      <Content
-        content={[
-          { part: part1, exercises: exercises1 },
-          { part: part2, exercises: exercises2 },
-          { part: part3, exercises: exercises3 },
-        ]}
-      />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </>
   );
 };
