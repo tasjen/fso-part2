82b3931bba7291c5f164aa31c23784f4bade8c1b
tastas000 <TiiLg0o000@gmail.com>
2023-12-15 11:42:10 +0700

    finish ex1.2

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -3,18 +3,15 @@ const Header = (props) => {
 };
 const Content = (props) => {
   return (
-    <>
-      <p>
-        {props.parts[0]} {props.exercises[0]}
-      </p>
-      <p>
-        {props.parts[1]} {props.exercises[1]}
-      </p>
-      <p>
-        {props.parts[2]} {props.exercises[2]}
-      </p>
-    </>
+    <div>
+      <Part part={props.content[0].part} exercises={props.content[0].exercises} />
+      <Part part={props.content[1].part} exercises={props.content[1].exercises} />
+      <Part part={props.content[2].part} exercises={props.content[2].exercises} />
+    </div>
   );
+};
+const Part = (props) => {
+  return <p>{props.part} {props.exercises}</p>;
 };
 const Total = (props) => {
   return <p>Number of exercises {props.total}</p>;
@@ -33,8 +30,11 @@ const App = () => {
     <>
       <Header course={course} />
       <Content
-        parts={[part1, part2, part3]}
-        exercises={[exercises1, exercises2, exercises3]}
+        content={[
+          { part: part1, exercises: exercises1 },
+          { part: part2, exercises: exercises2 },
+          { part: part3, exercises: exercises3 },
+        ]}
       />
       <Total total={exercises1 + exercises2 + exercises3} />
     </>
