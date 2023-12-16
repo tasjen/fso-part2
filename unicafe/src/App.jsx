f736afabbc8e63b7967a6cee8ffdd8c910f544df
tastas000 <TiiLg0o000@gmail.com>
2023-12-16 16:09:46 +0700

    fix warning 1.11

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -10,12 +10,14 @@ const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad;
   return all !== 0 ? (
     <table>
-      <StatisticLine text='good' value={good} />
-      <StatisticLine text='neutral' value={neutral} />
-      <StatisticLine text='bad' value={bad} />
-      <StatisticLine text='all' value={all} />
-      <StatisticLine text='average' value={(good - bad) / all || 0} />
-      <StatisticLine text='positive' value={((100 * good) / all || 0) + ' %'} />
+      <tbody>
+        <StatisticLine text='good' value={good} />
+        <StatisticLine text='neutral' value={neutral} />
+        <StatisticLine text='bad' value={bad} />
+        <StatisticLine text='all' value={all} />
+        <StatisticLine text='average' value={(good - bad) / all || 0} />
+        <StatisticLine text='positive' value={((100 * good) / all || 0) + ' %'} />
+      </tbody>
     </table>
   )
   : <p>No feedback given</p>
