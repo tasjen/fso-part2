96465241747ea912294dd861ce34c70b4cd966c7
tastas000 <TiiLg0o000@gmail.com>
2023-12-17 10:07:14 +0700

    finish 2.6

---
+++ phonebook/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    setPersons(persons.concat({name: newName}));
+    setNewName('');
+
+  }
+  const handleNewName = (event) => {
+    setNewName(event.target.value);
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNewName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person =>
+        <p key={person.name}>
+          {person.name}
+        </p>)}
+    </div>
+  )
+}
+
+export default App
